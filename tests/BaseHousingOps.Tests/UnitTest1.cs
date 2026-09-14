8a1fef96d24fd9ba95faf6c9d7f1222e9c01c51f
Naomi Nicole Donato <naomidonato@Naomis-MacBook-Pro.local>
2026-09-13 21:10:24 -0700

    Unit testing Person class methods and variables

--- tests/BaseHousingOps.Tests/UnitTest1.cs
+++ tests/BaseHousingOps.Tests/UnitTest1.cs
@@ -1,10 +1,97 @@
-﻿namespace BaseHousingOps.Tests;
+﻿using System.Reflection;
+using BaseHousingOps.Domain.Entities;
 
-public class UnitTest1
+namespace BaseHousingOps.Tests;
+
+//Testing that residents can be created.
+//Your test's expectation does not equal the application's actual behavior.
+public class ResidentTests
 {
     [Fact]
-    public void Test1()
+    public void Constructor_ShouldCreateResident()
     {
-
+        // Arange
+        var resident = new Resident (
+            "John",
+            "Smith",
+            "john@example.com",
+            "Navy");
+        
+        // Assert
+        Assert.Equal("John", resident.FirstName); //Failure: Strings differ, means the assertion failed
+        Assert.Equal("Smith", resident.LastName);
+        Assert.Equal("john@example.com", resident.Email);
+        Assert.Equal("Navy", resident.BranchOfService);
     }
+
+    //We are saying "I expect this code to throw an ArgumentExeption."
+    //Create invalid Resident -> Does it throw ArgumentException? -> YES -> TEST PASS
+    //If Resident does not throw an exception: Create invalid Resident -> No exception -> Test FAIL
+    [Fact]
+    public void Constructor_ShouldRejectEmptyFirstName()
+    {
+        //Act & Assert
+
+        //I EXPECT an ArgumentException.
+        Assert.Throws<ArgumentException>(() => //run this code
+            new Resident(
+                "",
+                "Smith",
+                "john@example.com",
+                "Navy"));
+    }
+
+    [Fact]
+
+    public void FullName_ShouldCombineFirstAndLastName()
+    {
+        //Arange
+        var resident = new Resident (
+            "John",
+            "Smith",
+            "john@example.com",
+            "Navy");
+        
+        //Act
+        var fullName = resident.FullName;
+
+        //Assert
+        Assert.Equal("John Smith", fullName);
+    }
+
+    [Fact]
+    public void Constructor_ShouldRejectEmptyLastName()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new Resident(
+                "John",
+                "",
+                "john@example.com",
+                "Navy"));
+    }
+
+    [Fact]
+    public void Constructor_ShouldRejectEmptyEmail()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new Resident(
+                "John",
+                "Smith",
+                "",
+                "Navy"));
+    }
+
+    [Fact]
+    public void Constructor_ShouldRejectInvalidEmail()
+    {
+        Assert.Throws<ArgumentException>(() =>
+            new Resident(
+                "John",
+                "Smith",
+                "john.example.com",
+                "Navy"));
+    }
+
+
+    
 }
